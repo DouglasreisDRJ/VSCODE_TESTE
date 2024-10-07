b41032cfe0010a11b324d6490ac9908fa9e888b6
DouglasreisDRJ <douglas.reis.jesus@gmail.com>
2024-10-07 19:43:47 -0400

    teste01

--- meu_modulo/main.py
+++ meu_modulo/main.py
@@ -1,4 +1,5 @@
 from basicos import somar, subtrair, dividir, mutiplica
+from avancados import quadrado, cubo
 
 
 
@@ -9,3 +10,4 @@ print(f"soma: {somar(a, b)}")
 print(f"subtracao: {subtrair(a, b)}")
 print(f"divisao: {dividir(a, b)}")
 print(f"mutiplicacao: {mutiplica(a, b)}")
+print(f"quadrado: {quadrado(a)}")
